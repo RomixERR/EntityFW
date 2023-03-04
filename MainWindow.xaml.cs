35899822ef3d7f2d7103a50dad42b6bff6b7f988
RomixERR <romixerr@mail.ru>
2023-03-04 17:26:53 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.Entity;
+
+namespace EntityFW
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        CustomersDatabaseEntities context = new CustomersDatabaseEntities();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void loadBtn_Click(object sender, RoutedEventArgs e)
+        {
+            context.Customers.Load();
+            dataGridCustomers.ItemsSource = context.Customers.Local.ToBindingList();
+        }
+
+    }
+}
